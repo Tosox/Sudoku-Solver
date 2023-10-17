dce8244429abf7d20d49cbedd2d15a453b6217d9
TosoxDev <57193602+TosoxDev@users.noreply.github.com>
2023-10-17 15:58:52 +0200

    fix: file name

--- examples/Sudoku-Solver-Helper.bat
+++ examples/Sudoku-Solver-Helper.bat
@@ -3,6 +3,6 @@ title Sudoku Solver
 
 set arg1=%1
 if not defined arg1 set /p arg1="Please provide a path to the Sudoku file: "
-java -jar "./Sudoku-Solver_v0.0.1.jar" %arg1%
+java -jar "./Sudoku-Solver_v1.0.0.jar" %arg1%
 
 pause
